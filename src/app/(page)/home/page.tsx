3f5ecb1bdd00adf8da091dfe97160af8ffa067d5
DE-Uehara <r.uehara@defide-ix.com>
2024-07-29 09:47:06 +0900

    こみこみ

---
+++ src/app/(page)/home/page.tsx
@@ -0,0 +1,23 @@
+import { SignOut } from "@/app/components/SignOut";
+import { auth } from "@/auth";
+import React from "react";
+import Image from "next/image";
+
+const page = async () => {
+  const session = await auth();
+  if (session === null) return "";
+  return (
+    <div>
+      <SignOut />
+      {session!.user!.name}
+      <Image
+        src={session!.user!.image!}
+        width={20}
+        height={20}
+        alt={session!.user!.name!}
+      />
+    </div>
+  );
+};
+
+export default page;
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,13 +1,5 @@
-import { auth } from "@/auth"
+export { auth as middleware } from "@/auth"
 
-export default auth((req) => {
-  if (!req.auth && req.nextUrl.pathname !== "/hello-Henn") {
-    const newUrl = new URL("/hello-Henn", req.nextUrl.origin)
-    return Response.redirect(newUrl)
-  }
-})
-
-// Optionally, don't invoke Middleware on some paths
 export const config = {
   matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
-}
+};
