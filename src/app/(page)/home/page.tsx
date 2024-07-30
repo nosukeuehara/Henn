56132509769ce4626d907e9e25745e6eb06922b6
DE-Uehara <r.uehara@defide-ix.com>
2024-07-30 15:56:50 +0900

    auth編集

--- src/app/(page)/home/page.tsx
+++ src/app/(page)/home/page.tsx
@@ -1,8 +1,11 @@
+import { auth } from "@/auth";
+import SessionData from "@/components/test";
 import { createClient } from "@/utils/supabase/client";
 
 const page = async () => {
   const supabase = await createClient();
   let { data, error } = await supabase.from("posts-db").select("*");
+  const session = await auth();
   return (
     <div>
       {data?.map((d) => {
@@ -13,6 +16,7 @@ const page = async () => {
           </div>
         );
       })}
+      <SessionData session={session} />
     </div>
   );
 };
--- src/auth.ts
+++ src/auth.ts
@@ -16,6 +16,38 @@ export const config: NextAuthConfig = {
   adapter: SupabaseAdapter({
     url: process.env.NEXT_PUBLIC_SUPABASE_URL!,
     secret: process.env.SUPABASE_SERVICE_ROLE_KEY!,
-  })
+  }),
+  callbacks: {
+    authorized({ request, auth }) {
+      const { pathname } = request.nextUrl
+      if (pathname === "/middleware-example") return !!auth
+      return true
+    },
+    jwt({ token, trigger, session, account }) {
+      if (trigger === "update") token.name = session.user.name
+      if (account?.provider === "keycloak") {
+        return { ...token, accessToken: account.access_token }
+      }
+      return token
+    },
+    async session({ session, token }) {
+      if (token?.accessToken) {
+        session.accessToken = token.accessToken
+      }
+      return session
+    },
+  },
 }
 export const { handlers, auth, signIn, signOut } = NextAuth(config)
+
+declare module "next-auth" {
+  interface Session {
+    accessToken?: string
+  }
+}
+
+declare module "next-auth/jwt" {
+  interface JWT {
+    accessToken?: string
+  }
+}
